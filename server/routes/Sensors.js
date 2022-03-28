9195fd3a79a911a027e61d443095021cce5e0428
Dave-Sama <davech1228@gmail.com>
2022-03-28 21:45:07 +0300

    added a route folder and sensors.js

---
+++ server/routes/Sensors.js
@@ -0,0 +1,65 @@
+const request = require('request');
+const router = require('express').Router();
+
+router.get('/get-data/:sample', async (req, res) => {
+	const sampleNum = req.params.sample;
+
+	request.get(
+		{
+			url: `https://beezone-b24ac-default-rtdb.europe-west1.firebasedatabase.app/${sampleNum}.json`,
+			json: {},
+		},
+		(err, response, body) => {
+			if (err) {
+				return res.status(err.statusCode).send(err);
+			} else if (response.statusCode === 200) {
+				//Organize the relevant details
+				const data = {
+					temperature: response.body.T85,
+					humidity: response.body.RH85,
+				};
+				console.log(data);
+				return res.status(200).send(JSON.stringify(data));
+			} else {
+				res.status(400).send();
+			}
+		}
+	);
+});
+router.get('/get-data', async (req, res) => {
+	// const sampleNum = req.params.sample;
+
+	request.get(
+		{
+			url: `https://beezone-b24ac-default-rtdb.europe-west1.firebasedatabase.app/.json`,
+			json: {},
+		},
+		(err, response, body) => {
+			if (err) {
+				return res.status(err.statusCode).send(err);
+			} else if (response.statusCode === 200) {
+				let data = response.body;
+
+				// delete unnessesery data. leave only humidity and temperature data.
+				for (let i = 0; i < data.length; ++i) {
+					delete data[i]['AT85'];
+					delete data[i]['AT85-ATamb'];
+					delete data[i]['ATamb'];
+					delete data[i]['DateTime'];
+					delete data[i]['RHamb'];
+					delete data[i]['T85-Tamb'];
+					delete data[i]['Tamb'];
+				}
+				return res.status(200).send(Object.assign({}, data));
+			} else {
+				res.status(400).send();
+			}
+		}
+	);
+});
+
+router.get('/*', async (req, res) => {
+	res.send('BeeZone server');
+});
+
+module.exports = router;
