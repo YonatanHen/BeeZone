3ee9beb8cc03ceb31f58a5fe0945003e83d78fcd
YehonatanHen <yonatan.h333@gmail.com>
2022-03-28 16:43:46 +0300

    data fetched

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const db = require('./database/db')
+const request= require('request')
 const app = express()
 
 app.use(express.json())
@@ -12,6 +12,22 @@ const port = process.env.port || 8000
 
 app.listen(port, () => {
     console.log('app is listen to port ' + port )
+})
+
+app.get('/get-temperature', async (req,res) => {
+    request.get({
+        url: `https://beezone-b24ac-default-rtdb.europe-west1.firebasedatabase.app/0.json`,
+        json: {}
+    }, (err, response, body) => {
+        if (err) {
+            return res.status(400).send(err);
+        } else if (response.statusCode === 200) {
+            console.log(response.body)
+            return res.status(200).send()
+        } else {
+            console.log(response.statusCode);
+        }
+    })
 })
 
 app.get('/*', async (req, res) => {
