9a91954d7d342d28e7ab39fbfc5c15f7226f88b0
Ariel Turchinsky <ariel7590@gmail.com>
2022-04-02 20:29:56 +0300

    Added a sign-in and a sign-up template and customed it for the theme of the website

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { Dashboard } from './dashboard';
+import SignIn from './sign-in/sign-in';
 
 function App() {
   return (
     <div>
-      <div>
+      {/* <div>
         <h1>Dashboard & charts goes here!</h1>
-      </div>
-      <Dashboard />
+      </div> */}
+      {/* <Dashboard /> */}
+      <SignIn />
     </div>
 
   );
