745951686c81fc1c3182e1fa8d3b67a1b837cfae
Dave-Sama <davech1228@gmail.com>
2022-04-12 12:02:29 +0300

    added normal styling tools - hate to work like an arab

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import { Dashboard } from './dashboard';
 import SignIn from './sign-in/sign-in';
 
 function App() {
-  return (
-    <div>
-      {/* <div>
-        <h1>Dashboard & charts goes here!</h1>
-      </div> */}
-      {/* <Dashboard /> */}
-      <SignIn />
-    </div>
+	return (
+		<div>
+			<div>
+				<h1>Dashboard & charts goes here!</h1>
+			</div>
 
-  );
+			<Dashboard />
+			{/* <SignIn /> */}
+		</div>
+	);
 }
 
 export default App;
