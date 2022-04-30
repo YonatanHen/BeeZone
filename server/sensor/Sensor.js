920620918019b0346e34883fe465c1e75dc47244
Dave-Sama <davech1228@gmail.com>
2022-05-01 01:41:03 +0300

    added the data fetching from the sensors to the server

---
+++ server/sensor/Sensor.js
@@ -0,0 +1,21 @@
+var SerialPort = require('serialport');
+const parsers = SerialPort.parsers;
+
+const parser = new parsers.Readline({
+	delimiter: '\r\n',
+});
+
+// If needed, change my port to your Arduino Port.
+var port = new SerialPort('/COM4', {
+	baudRate: 9600,
+	dataBits: 8,
+	parity: 'none',
+	stopBits: 1,
+	flowControl: false,
+});
+
+port.pipe(parser);
+
+parser.on('data', function (data) {
+	console.log('Received data from port: ' + data);
+});
