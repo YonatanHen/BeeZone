7202fc605bbd6f9acfd78051289e75d0fd8bf214
Dave-Sama <davech1228@gmail.com>
2022-05-07 15:24:38 +0300

    added the machine learning algorithm as a middleware

--- server/ML/learn.js
+++ server/ML/learn.js
@@ -4,30 +4,26 @@ const {
 } = require('@tensorflow/tfjs-layers/dist/engine/training');
 
 require('@tensorflow/tfjs-node');
+
+let isOk = true;
+
 // my data:
 const sensor = require('./sensor.json');
 
+// const trainProcess = () => {
 // train data
 const data = sensor.map((item) => [item.RH85, item.T85]);
 // console.log(data);
-
-// testing data:
-const testingData = [100, 33.8];
-
 const trainData = tf.tensor2d(data);
-
-// output data
+// output data - what we would want to get
 const outputData = tf.tensor2d(
 	sensor.map((item) => {
 		// good
-		if (item.RH85 >= 50 && item.T85 >= 30) return [1, 0];
+		if (item.RH85 >= 50 && item.T85 >= 20) return [1, 0];
 		// bad
 		else return [0, 1];
 	})
 );
-
-// test data
-const test = tf.tensor2d([[testingData[0], testingData[1]]]);
 
 // build neural network
 let model = tf.sequential();
@@ -55,123 +51,34 @@ model.compile({
 	loss: 'meanSquaredError',
 	optimizer: tf.train.adam(0.06),
 });
+// };
+
+// test data
+const getTestData = (testingData) => {
+	return (test = tf.tensor2d([[testingData[0], testingData[1]]]));
+};
 
 // train/fit our network
 
-const startTime = Date.now();
-model
-	.fit(trainData, outputData, { epochs: 20 })
-	.then((history) => {
-		console.log('data', Date.now() - startTime, 'ms');
-		result = model.predict(test).data();
+const checkData = (data) => {
+	const startTime = Date.now();
+	model
+		.fit(trainData, outputData, { epochs: 20 })
+		.then((history) => {
+			console.log('data', Date.now() - startTime, 'ms');
+			result = model.predict(data).data();
 
-		// the final result.
-		result.then((data) => console.log(data));
-	})
-	.catch((err) => console.log('error:', err));
+			// the final result.
+			result.then((data) => {
+				if (data[0] > 0.5 || NaN) {
+					isOk = true;
+				} else if (data[0] < 0.5) {
+					isOk = false;
+				}
+				// console.log(data[0], isOk);
+			});
+		})
+		.catch((err) => console.log('error:', err));
+};
 
-// export class ML {
-// 	model = tf.sequential();
-// 	testingData;
-// 	train;
-// 	output;
-// 	test;
-// 	result;
-
-// 	constructor(testingData) {
-// 		this.testingData = testingData;
-// 	}
-// 	trainData = () => {
-// 		let data = sensor.map((item) => [item.RH85, item.T85]);
-// 		this.train = tf.tensor2d(data);
-// 	};
-
-// 	outputData = () => {
-// 		this.output = tf.tensor2d(
-// 			sensor.map((item) => {
-// 				// good
-// 				if (item.RH85 >= 50 && item.T85 >= 30) {
-// 					return [1, 0];
-// 					// bad
-// 				} else {
-// 					return [0, 1];
-// 				}
-// 			})
-// 		);
-// 	};
-
-// 	testData = () => {
-// 		// console.log(this.testingData);
-// 		this.test = tf.tensor2d([
-// 			[this.testingData['RH85'], this.testingData['T85']],
-// 		]);
-// 	};
-
-// 	buildNN = () => {
-// 		// build neural network
-
-// 		this.model.add(
-// 			tf.layers.dense({
-// 				inputShape: [2],
-// 				activation: 'sigmoid',
-// 				units: 5,
-// 			})
-// 		);
-// 		this.model.add(
-// 			tf.layers.dense({
-// 				inputShape: [5],
-// 				activation: 'relu',
-// 				units: 2,
-// 			})
-// 		);
-// 		this.model.add(
-// 			tf.layers.dense({
-// 				activation: 'softmax',
-// 				units: 2,
-// 			})
-// 		);
-// 		this.model.compile({
-// 			loss: 'meanSquaredError',
-// 			optimizer: tf.train.adam(0.06),
-// 		});
-// 	};
-
-// 	// train/fit our network
-// 	fitModel = async () => {
-// 		this.trainData();
-// 		this.outputData();
-// 		this.testData();
-
-// 		const startTime = Date.now();
-// 		this.model
-// 			.fit(this.train, this.output, { epochs: 20 })
-// 			.then(async (history) => {
-// 				console.log('data', Date.now() - startTime, 'ms');
-// 				this.result = await this.model.predict(this.test).data();
-
-// 				// the final result.
-// 				console.log(this.result);
-// 			})
-// 			.catch((err) => console.log('error:', err));
-// 	};
-
-// 	getResult = () => {
-// 		try {
-// 			this.result;
-// 		} catch (e) {
-// 			console.error('err', e);
-// 		}
-// 	};
-
-// 	run = () => {
-// 		this.buildNN();
-// 		this.fitModel();
-// 	};
-// }
-
-// const ml = new ML({
-// 	RH85: 100,
-// 	T85: 33.8,
-// });
-
-// ml.run();
+module.exports = { getTestData, checkData, isOk };
