76826666c51ec6bd67d2ec96a10f98c39cd16fe0
Dave-Sama <davech1228@gmail.com>
2022-05-08 15:14:07 +0300

    finito dashboard

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -6,11 +6,28 @@ import { Link } from 'react-router-dom';
 import { userService } from '../../services/user.service';
 import { GiTreeBeehive } from 'react-icons/gi';
 import '../dashboard/dashboard.css';
+import Modal from 'react-modal';
 
 let num = 0;
-
+const customStyles = {
+	content: {
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		width: '20%',
+		height: '40%',
+		transform: 'translate(-50%, -50%)',
+		backgroundImage: "url('https://wallpaperaccess.com/full/1634369.jpg')",
+		backgroundSize: 'cover',
+		backgroundColor: 'rgba(255, 239, 226,1)',
+		backgroundBlendMode: 'overlay',
+	},
+};
 export const Dashboard = (props) => {
 	const [ScreenshootClicked, setScreenshootClicked] = useState(false);
+	const [showModal, setShowModal] = useState(false);
 
 	const [tempData, setTempData] = useState({
 		labels: [],
@@ -42,6 +59,14 @@ export const Dashboard = (props) => {
 	const [temperatures, setTemperatures] = useState([]);
 	const [humidities, setHumidities] = useState([]);
 	const [clicked, setClicked] = useState(false);
+
+	const openModal = () => {
+		setShowModal(true);
+	};
+
+	const closeModal = () => {
+		setShowModal(false);
+	};
 
 	useEffect(() => {
 		const interval = setInterval(async () => {
@@ -95,6 +120,13 @@ export const Dashboard = (props) => {
 		window.location.reload(true);
 	};
 
+	function afterOpenModal() {
+		// references are now sync'd and can be accessed.
+		subtitle.style.color = '#fff';
+	}
+
+	let subtitle;
+
 	return (
 		<div>
 			<div className='container'>
@@ -110,9 +142,9 @@ export const Dashboard = (props) => {
 						{/* <Link className='button' to='/dashboard'>
 							Refresh Page
 						</Link> */}
-						<Link className='button' to='/about'>
+						<a className='button' onClick={openModal}>
 							Edit Profile
-						</Link>
+						</a>
 						<button
 							className='button'
 							onClick={() => {
@@ -139,6 +171,107 @@ export const Dashboard = (props) => {
 					)}
 					{clicked && <Webcam className='webcam' audio={false} />}
 				</div>
+				<Modal
+					isOpen={showModal}
+					onAfterOpen={afterOpenModal}
+					onRequestClose={closeModal}
+					style={customStyles}
+					contentLabel='Example Modal'
+				>
+					<h2
+						style={{
+							width: '90%',
+							marginLeft: '0.9rem',
+							marginBottom: '1.2rem',
+							backgroundColor: 'rgba(255, 224, 110,1)',
+							color: 'white',
+							textAlign: 'center',
+							height: '3vh',
+							marginBottom: '1rem',
+							fontSize: '1rem',
+						}}
+						ref={(_subtitle) => (subtitle = _subtitle)}
+					>
+						EDIT PERSONAL INFORMATION
+					</h2>
+					{/* <button
+						style={{
+							// backgroundColor: 'red',
+							float: 'right',
+							width: '1.4rem',
+							boxShadow: '0px 0px 0px 1px black',
+							borderRadius: '2px',
+						}}
+						onClick={closeModal}
+					>
+						X
+					</button> */}
+					<div
+						style={{
+							// backgroundColor: 'red',
+							width: '90%',
+							height: '80%',
+							margin: 'auto',
+							display: 'flex',
+							flexDirection: 'column',
+							background: 'rgba(255, 224, 110,0.5)',
+						}}
+					>
+						<form>
+							<label
+								style={{
+									display: 'block',
+									marginLeft: '.9rem',
+									paddingTop: '.9rem',
+								}}
+								htmlFor='name'
+							>
+								User Name:
+							</label>
+							<input
+								id='name'
+								style={{
+									width: '90%',
+									marginLeft: '.9rem',
+								}}
+							/>
+
+							<label
+								style={{ display: 'block', marginLeft: '.9rem' }}
+								htmlFor='password'
+							>
+								Password:
+							</label>
+							<input
+								id='password'
+								style={{ width: '90%', marginLeft: '.9rem' }}
+							/>
+
+							<label
+								style={{ display: 'block', marginLeft: '.9rem' }}
+								htmlFor='email'
+							>
+								Email:
+							</label>
+							<input
+								type='email'
+								id='email'
+								style={{ width: '90%', marginLeft: '.9rem' }}
+							/>
+							<div style={{ display: 'flex', height: '40%' }}>
+								<button
+									style={{
+										backgroundColor: 'white',
+										padding: '0.5rem',
+										margin: 'auto',
+									}}
+								>
+									Submit
+								</button>
+							</div>
+						</form>
+					</div>
+				</Modal>
 				<div className='dashboard-container'>
 					<Chart
 						className='chart'
